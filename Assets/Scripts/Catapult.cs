19d6880b1326e04d5e7760f058d21a4717523136
Lukas <Lukasalfridsson@hotmail.se>
2024-05-15 18:52:44 +0200

    katapult

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -5,23 +5,64 @@ using UnityEngine;
 public class Catapult : MonoBehaviour
 {
     public Transform firePoint;
+    public Transform castPoint;
     public GameObject ProjectileHayball;
+    public Transform enemy;
 
+    public float attackRange;
     public float attackRate = 2f;
     float nextAttackTime = 0f;
+
+    bool Detected = false;
+
+    Vector2 Direction;
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time >= nextAttackTime)
+        Vector2 enemyPos = enemy.position;
+
+        Direction = enemyPos - (Vector2)transform.position;
+
+        RaycastHit2D rayInfo = Physics2D.Raycast(transform.position, Direction, attackRange);
+
+        if (rayInfo)
         {
-            if (Input.GetButtonDown("Fire2"))
+            
+            if(rayInfo.collider.gameObject.tag == "Enemy")
             {
+                if (Detected ==  false)
+                {
+                    Detected = true;
+                    
+                }
+            }
+
+            else
+            {
+                if (Detected == true)
+                {
+                    Detected = false;
+                    
+                }
+            }           
+        }
+
+        if (Detected)
+        {
+            if (Time.time >= nextAttackTime)
+            {                             
                 Shoot();
-                nextAttackTime = Time.time + 1f / attackRate;
+                nextAttackTime = Time.time + 1f / attackRate;               
             }
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+
     void Shoot()
     {
         Instantiate(ProjectileHayball, firePoint.position, firePoint.rotation);
