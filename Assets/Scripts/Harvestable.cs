26b8e847d338cb06346bb1b2f51a3f364558cdae
Siamreach <123168957+Siamreach@users.noreply.github.com>
2024-05-24 18:25:49 +0200

    Spawnar lögs

--- Assets/Scripts/Harvestable.cs
+++ Assets/Scripts/Harvestable.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class Harvestable : MonoBehaviour
 {
-	public void Harvest()
+	[field: SerializeField] public int ResourceCount { get; private set; }
+
+	[field: SerializeField] public ParticleSystem ResourceEmitPS { get; private set; }
+	private int _amountHarvested = 0;
+
+	public void Harvest(int amount)
 	{
-		Destroy(gameObject);
+		int amountToSpawn = Mathf.Min(amount, ResourceCount - _amountHarvested);
+
+		if (amountToSpawn > 0)
+		{
+			ResourceEmitPS.Emit(amountToSpawn);
+			_amountHarvested += amountToSpawn;
+		}
+
+		if (_amountHarvested >= ResourceCount)
+		{
+			Destroy(gameObject);
+		}
+		
 	}
 }
